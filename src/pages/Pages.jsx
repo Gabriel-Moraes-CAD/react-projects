069d348d00f6ea6a9fbda96e2cb8d46df3ae18cc
Gabriel <mr.gabriel.moraes@gmail.com>
2022-09-28 19:57:52 -0400

    hashroute updates

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import {Route, Routes} from "react-router-dom";
-import MainPage from '../components/MainPage';
-import SpaceProFigGridDetails from '../components/SpaceProFigGridDetails';
-import SpaceProMain from '../components/SpaceProMain';
-import SpaceProSpaceGDetails from '../components/SpaceProSpaceGDetails';
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import MainPage from "../components/MainPage";
+import SpaceProFigGridDetails from "../components/SpaceProFigGridDetails";
+import SpaceProMain from "../components/SpaceProMain";
+import SpaceProSpaceGDetails from "../components/SpaceProSpaceGDetails";
 
 function Pages() {
   return (
-     <Routes>
-            <Route path="/" element={<MainPage/>}/>
-            <Route path="/spaceProMain" element={<SpaceProMain/>}/>
-            <Route path="/spaceprospacegdetails/:image" element={<SpaceProSpaceGDetails/>}/>
-            <Route path="/spaceprofiggriddetails/:id" element={<SpaceProFigGridDetails/>}/>
+    <Routes>
+      <Route path="/nasa-pro" element={<MainPage />} />
+      <Route path="/spaceProMain" element={<SpaceProMain />} />
+      <Route
+        path="/spaceprospacegdetails/:image"
+        element={<SpaceProSpaceGDetails />}
+      />
+      <Route
+        path="/spaceprofiggriddetails/:id"
+        element={<SpaceProFigGridDetails />}
+      />
     </Routes>
-  )
+  );
 }
 
-export default Pages
+export default Pages;
