261d8aa0122d63b320d8c72cb855e8ae0aaf56c8
Gabriel <mr.gabriel.moraes@gmail.com>
2022-09-28 20:32:01 -0400

    ?

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/nasa-pro">
         
         <Pages/>
     
